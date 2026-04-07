378f2a57bc1b7fa3ae55572a0932819ae527c6be
Benoit Rolandeau <benoit.rolandeau@allcircuits.com>
2026-04-07 12:41:04 +0000

    fix: Change keys parameter type from List to Set in StringIntervalUtility and TextUtility for improved performance and to avoid empty keys

--- act_dart_utility/lib/src/utilities/string_interval_utility.dart
+++ act_dart_utility/lib/src/utilities/string_interval_utility.dart
@@ -13,7 +13,7 @@ sealed class StringIntervalUtility {
   /// the list of transformed intervals in order to merge them into a single element.
   static FinalElement actOnInterval<FinalElement, SubElement>(
     String text,
-    List<String> keys,
+    Set<String> keys,
     SubElement Function(StringInterval interval) transform,
     FinalElement Function(List<SubElement> elements) merge,
   ) {
@@ -35,7 +35,9 @@ sealed class StringIntervalUtility {
   /// key in the list order will prevail for the interval.
   /// For instance, for the keys: ["ber"] and text: "Cumbersome" this will creates three intervals:
   /// [ cum, ber, some ]
-  static List<StringInterval> getIntervals(String text, List<String> keys) =>
+  ///
+  /// Empty key is ignored, otherwise it would create too much intervals.
+  static List<StringInterval> getIntervals(String text, Set<String> keys) =>
       flatIntervals(text, keys, findIntervals(text, keys));
 
   /// This methods finds all the string intervals thanks to the keys given
@@ -43,13 +45,20 @@ sealed class StringIntervalUtility {
   /// The intervals can intersects between themselves
   /// For instance and for the keys: [ "ber"] and text: "Cumbersome" this will creates one
   /// interval: [ "ber" ]
+  ///
+  /// Empty key is ignored, otherwise it would create too much intervals.
   static Map<String, List<StringInterval>> findIntervals(
     String text,
-    List<String> keys,
+    Set<String> keys,
   ) {
     final textIntervals = <String, List<StringInterval>>{};
 
     for (final key in keys) {
+      if (key.isEmpty) {
+        // We don't want to find empty key, it would create too much intervals
+        continue;
+      }
+
       var idx = 0;
       final length = key.length;
       textIntervals[key] = [];
@@ -81,7 +90,7 @@ sealed class StringIntervalUtility {
   /// After the call of this method, every character is in one and only one interval
   static List<StringInterval> flatIntervals(
     String text,
-    List<String> keys,
+    Set<String> keys,
     Map<String, List<StringInterval>> intervalsToFlat,
   ) {
     final flattenTextIntervals = <StringInterval>[];
