08d8414224cc8a5af34da7fd51e32adfdecaa89d
Benoit Rolandeau <benoit.rolandeau@allcircuits.com>
2026-04-14 16:38:22 +0000

    feat: add native event listener services and update dependencies

---
+++ act_ffi_utility/lib/src/services/abs_native_event_listener_service.dart
@@ -0,0 +1,154 @@
+// SPDX-FileCopyrightText: 2026 Benoit Rolandeau <benoit.rolandeau@allcircuits.com>
+//
+// SPDX-License-Identifier: LicenseRef-ALLCircuits-ACT-1.1
+
+import 'dart:async';
+import 'dart:ffi' as ffi;
+
+import 'package:act_abstract_manager/act_abstract_manager.dart';
+import 'package:act_dart_utility/act_dart_utility.dart';
+import 'package:act_ffi_utility/src/utilities/runtime_protect_cmd.dart';
+import 'package:act_global_manager/act_global_manager.dart';
+import 'package:act_logger_manager/act_logger_manager.dart';
+import 'package:flutter/foundation.dart';
+
+/// This is the callback signature for the method used to register the native callback in the C
+/// library.
+typedef RegisterCallback<Result extends MixinResultStatus, Callback extends Function> =
+    Result Function(ffi.Pointer<ffi.NativeFunction<Callback>>);
+
+/// Abstract service for listening to native events.
+///
+/// This service provides a way to listen to native events and parse them into Dart objects.
+///
+/// The service is designed to be extended by specific implementations for different types of native
+/// events.
+abstract class AbsNativeEventListenerService<
+  Result extends MixinResultStatus,
+  Callback extends Function,
+  ParsedObject
+>
+    extends AbsWithLifeCycle {
+  /// Logs helper for this service.
+  final LogsHelper logsHelper;
+
+  /// Stream controller for the parsed objects received from the native callback.
+  final StreamController<ParsedObject> _streamController;
+
+  /// The callback used to register the native callback in the C library. It should be provided by
+  /// the subclass. See [getRegisterCallback].
+  late final RegisterCallback<Result, Callback> _registerNativeCallback;
+
+  /// The current value of the parsed object received from the native callback.
+  ParsedObject? _currentValue;
+
+  /// The native callable registered with the C library.
+  ffi.NativeCallable<Callback>? _nativeCallable;
+
+  /// A broadcast stream of parsed objects received from the native callback.
+  Stream<ParsedObject> get eventStream => _streamController.stream;
+
+  /// The current value of the parsed object received from the native callback. It can be null if
+  /// no event has been received yet.
+  ParsedObject? get currentValue => _currentValue;
+
+  /// Sets the current value of the parsed object and adds it to the stream.
+  @protected
+  set currentValue(ParsedObject value) {
+    if (value == _currentValue) {
+      // Nothing to do
+      return;
+    }
+
+    _currentValue = value;
+    _streamController.add(value);
+  }
+
+  /// Class constructor
+  AbsNativeEventListenerService({
+    required String logsCategory,
+    required RegisterCallback<Result, Callback> registerNativeCallback,
+    LogsHelper? parentLogsHelper,
+  }) : logsHelper =
+           parentLogsHelper?.createASubLogsHelper(logsCategory) ??
+           LogsHelper(logsManager: globalGetIt().get<LoggerManager>(), logsCategory: logsCategory),
+       _streamController = StreamController<ParsedObject>.broadcast(),
+       _registerNativeCallback = registerNativeCallback;
+
+  /// {@macro act_abstract_manager.AbsWithLifeCycle.initLifeCycle}
+  @override
+  Future<void> initLifeCycle() async {
+    await super.initLifeCycle();
+
+    _registerNativeCallbackProtectCmd();
+
+    await _getValueFromGetterAndSetObject();
+  }
+
+  /// {@template act_ffi_utility.AbsNativeEventListenerService.getValueGetter}
+  /// Get a function that can be used to get the current value.
+  ///
+  /// If the method returns null, it means that we can't get the current value and we will only
+  /// update the value when we receive an event from the native callback.
+  /// {@endtemplate}
+  @protected
+  FutureOr<ParsedObject?> Function()? getValueGetter() => null;
+
+  /// {@template act_ffi_utility.AbsNativeEventListenerService.getNativeCallback}
+  /// Get the native callback to register in the C library. This callback will be called from the C
+  /// library when an event occurs. It should parse the event and update the current value using
+  /// the [currentValue] setter.
+  /// {@endtemplate}
+  @protected
+  ffi.NativeCallable<Callback> getNativeCallback();
+
+  /// Helper to get a description for the runtime protect command, including the logs category and a
+  /// specific action description.
+  @protected
+  String getDescriptionForRuntimeProtectCmd(String descriptionAction) =>
+      "${logsHelper.logsCategory} - $descriptionAction";
+
+  /// Helper to register the native callback in the C library, protected with [RuntimeProtectCmd] to
+  /// catch any exceptions and log them.
+  Result? _registerNativeCallbackProtectCmd() => RuntimeProtectCmd.protect<Result>(() {
+    _nativeCallable ??= getNativeCallback();
+
+    final result = _registerNativeCallback(_nativeCallable!.nativeFunction);
+    if (result.isError) {
+      logsHelper.e("Failed to register native callback: $result");
+    }
+
+    return result;
+  }, description: getDescriptionForRuntimeProtectCmd("registerNativeCallback"));
+
+  /// If a value getter is provided, use it to get the current value and set it in the service.
+  Future<void> _getValueFromGetterAndSetObject() async {
+    final valueGetter = getValueGetter();
+    if (valueGetter == null) {
+      return;
+    }
+
+    final initValue = await valueGetter();
+
+    if (initValue == null) {
+      // Nothing to do
+      return;
+    }
+
+    currentValue = initValue;
+  }
+
+  /// {@macro act_abstract_manager.AbsWithLifeCycle.disposeLifeCycle}
+  @override
+  Future<void> disposeLifeCycle() async {
+    // Unregister the native callback
+    _registerNativeCallback(ffi.nullptr);
+
+    _nativeCallable?.close();
+    _nativeCallable = null;
+
+    await _streamController.close();
+
+    return super.disposeLifeCycle();
+  }
+}
